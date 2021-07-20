8b7d9c5a1cde5231e74289fd0ad47fed8819db92
Aditya Mehrotra <aditya.mehrotra97@gmail.com>
2021-07-21 01:38:22 +0530

    Update Code.cpp

--- 27. Remove Element/Code.cpp
+++ 27. Remove Element/Code.cpp
@@ -1,7 +1,16 @@
 class Solution {
 public:
     int removeElement(vector<int>& nums, int val) {
-        
-        return 0;
+        int i = 0, n = nums.size();
+        while (i < n) {
+            if (nums[i] == val) {
+                nums[i] = nums[n - 1];
+                n--;
+            }
+            else{
+                i++;
+            }
+        }
+        return n;
     }
 };
