d63dde8a1960a4150af7784d59fbd25caba3d5f8
Aditya Mehrotra <aditya.mehrotra97@gmail.com>
2022-05-25 17:23:12 -0400

    Create Code.cpp

---
+++ 49. Group Anagrams/Code.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        unordered_map <string, vector<string>> mp;t
+        int n = strs.size();
+        string tmp;
+        
+        for(int i = 0; i < n; i++) {
+            tmp = strs[i];
+            sort(strs[i].begin(), strs[i].end());
+            mp[strs[i]].push_back(tmp);
+        }
+        
+        vector<vector<string>> result;    
+        for(auto itr = mp.begin(); itr != mp.end(); itr++) {
+            result.push_back(itr->second);
+        }    
+        return result;
+    }
+};
