863e1e6d48af1c3df53a8bd682eb8be61d21d7c8
Aditya Mehrotra <aditya.mehrotra97@gmail.com>
2023-07-10 20:20:24 -0400

    Create Code.cpp

---
+++ 763. Partition Labels/Code.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    vector<int> partitionLabels(string s) {
+        int n = s.size();
+
+        vector<int> last_index(26);
+        for (int i = 0; i < n; ++i) {
+            last_index[s[i] - 'a'] = i;
+        }
+
+        int size = 0;
+        int end = 0;
+        vector<int> result;
+        for (int i = 0; i < n; ++i) {
+            size++;
+            end = max(end, last_index[s[i] - 'a']);
+
+            if (i == end) {
+                result.push_back(size);
+                size = 0;
+            }
+        }
+
+        return result;
+    }
+};
